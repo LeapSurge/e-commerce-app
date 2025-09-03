9d84600cbdaad279e9ea0ccccb4825342c5c4f52
LeapSurge <LeapSurge@proton.me>
2025-09-04 00:57:45 +0800

    brand filter

--- src/routes/all-list.jsx
+++ src/routes/all-list.jsx
@@ -1,13 +1,13 @@
 import { Link, useSearchParams } from "react-router-dom";
-import { Pagination, Row, Col, Select, Flex } from "antd";
+import { Pagination, Row, Col, Select, Flex, Checkbox } from "antd";
 import BootCard from "../components/bootcard";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 async function fetchList(params) {
   const url = new URL("http://localhost:3000/boots");
 
   url.search = params.toString();
-  // console.log(url.search);
+  console.log(url.search);
 
   const response = await fetch(url);
 
@@ -17,7 +17,12 @@ async function fetchList(params) {
 export default function AllList() {
   const [boots, setBoots] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams();
-  const sortOrder = searchParams.get("_sort") || "default";
+  const sortOrder = searchParams.get("_order") || "default";
+  const brands = searchParams.getAll("brand");
+
+  const filters = useMemo(() => {
+    return { brand: brands };
+  }, [brands.join(",")]);
 
   useEffect(() => {
     let ignore = false;
@@ -27,20 +32,37 @@ export default function AllList() {
         setBoots(await fetchList(searchParams));
       }
     };
-
+    console.log(filters);
     loadList();
     return () => (ignore = true);
-  }, [searchParams]);
+  }, [sortOrder, filters]);
 
   return (
     <Flex vertical gap="small" className="all-list">
-      <Flex>
+      <Flex align="center" gap="middle">
+        <Flex className="filter--item">
+          <div className="title" gap="small">
+            品牌：
+          </div>
+          <Checkbox.Group
+            options={["Clarks", "Dr. Martens", "UGG"]}
+            value={brands}
+            onChange={(selected) => {
+              setSearchParams((p) => {
+                const newParams = new URLSearchParams(p);
+                newParams.delete("brand");
+                selected.forEach((brand) => newParams.append("brand", brand));
+                return newParams;
+              });
+            }}
+          />
+        </Flex>
         <Select
           value={sortOrder}
           options={[
             { label: "默认排序", value: "default" },
-            { label: "价格从高到低", value: "-price" },
-            { label: "价格从低到高", value: "price" },
+            { label: "价格从高到低", value: "desc" },
+            { label: "价格从低到高", value: "asc" },
           ]}
           onChange={(value) => {
             setSearchParams((prevParams) => {
@@ -49,9 +71,11 @@ export default function AllList() {
 
               // 2. 默认时删除_sort, 非默认添加_sort
               if (value !== "default") {
-                newParams.set("_sort", value);
+                newParams.set("_sort", "price");
+                newParams.set("_order", value);
               } else {
                 newParams.delete("_sort");
+                newParams.delete("_order");
               }
 
               // 3. 返回新的params对象
